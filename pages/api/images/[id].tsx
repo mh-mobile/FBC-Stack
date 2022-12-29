ea8deac6049a274b548e1225202134bdb9ab58ad
mh-mobile <mh.mobiler@gmail.com>
2022-12-29 21:31:43 +0900

    Puppeteerの代わりにPlaywrightで技術スタックの画面キャプチャを実装

--- pages/api/images/[id].tsx
+++ pages/api/images/[id].tsx
@@ -1,20 +1,36 @@
-import chromium from 'chrome-aws-lambda'
-import { scrollPageToBottom } from 'puppeteer-autoscroll-down'
-import puppeteer from 'puppeteer-core'
+import * as playwright from 'playwright-aws-lambda'
 import { getAllPostIds } from '../../../lib/posts'
 
-async function captureStack(url: string) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-  })
+// ref. https://swet.dena.com/entry/2018/04/26/152326
+async function scrollToBottom(page, viewportHeight) {
+  const getScrollHeight = () => {
+    return Promise.resolve(document.documentElement.scrollHeight)
+  }
 
-  const page = await browser.newPage()
-  await page.goto(url, {
-    waitUntil: 'networkidle0',
-    timeout: 5000,
+  let scrollHeight = await page.evaluate(getScrollHeight)
+  let currentPosition = 0
+  let scrollNumber = 0
+
+  while (currentPosition < scrollHeight) {
+    scrollNumber += 1
+    const nextPosition = scrollNumber * viewportHeight
+    await page.evaluate(function (scrollTo) {
+      return Promise.resolve(window.scrollTo(0, scrollTo))
+    }, nextPosition)
+    await page
+      .waitForNavigation({ waitUntil: 'networkidle2', timeout: 5000 })
+      .catch((e) => console.log('timeout exceed. proceed to next operation'))
+    currentPosition = nextPosition
+    scrollHeight = await page.evaluate(getScrollHeight)
+  }
+}
+
+async function captureStack(url: string) {
+  const browser = await playwright.launchChromium({
+    headless: true,
   })
+  const page = await browser.newPage()
+  await page.goto(url, { waitUntil: 'networkidle', timeout: 30000 })
 
   // 技術スタックのセレクターの領域を計算
   const clip = await page.evaluate((s) => {
@@ -28,14 +44,11 @@ async function captureStack(url: string) {
   await page.evaluate((_) => {
     window.scrollTo(0, 0)
   })
-  const lastPosition = await scrollPageToBottom(page, {
-    size: 400,
-    delay: 250,
-    stepsLimit: 50,
-  })
+  await scrollToBottom(page, 400)
 
   await page.waitForTimeout(1000)
-  const buffer = await page.screenshot({ clip })
+
+  const buffer = await page.locator('#stack').screenshot()
   browser.close()
   return buffer
 }
